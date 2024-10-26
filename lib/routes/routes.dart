be27d9aab17ca383864d3cd3a171fb937a5cf05f
Ruzana <ruza89@gmail.com>
2024-10-27 01:23:06 +0530

    minor change in Route class

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:machine_test_notifications/views/screens/home/home_screen.dart';
+import 'package:machine_test_notifications/views/screens/notification_screen.dart';
 
 class Routes{
 
   static final Map<String, WidgetBuilder> routes = {
     
     '/home':(context) => const HomeScreen(),
-    // '/notifications':(context)=> const NotificationScreen(), 
+    '/notifications':(context)=> const NotificationScreen(), 
   };
 }
